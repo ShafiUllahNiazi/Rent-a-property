6a36d1d3f6aa201ffb923c9d2e8aa896d6574036
ShafiUllahNiazi <shafiullahkhan117786@gmail.com>
2022-04-12 23:42:32 +0500

    rent a property added

---
+++ rent_property/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from rent_property.Views import AppartmentView, ContactUsView
+
+urlpatterns = [
+    path('apartment', AppartmentView.ApartmentView.as_view(), name='ApartmentView'),
+    path('apartment_list', AppartmentView.ApartmentListView.as_view(), name='ApartmentListView'),
+    path('apartment/<int:pk>', AppartmentView.ApartmrntDetailedView.as_view(), name='ApartmrntDetailedView'),
+    path('contact', ContactUsView.ContactUsView.as_view(), name='ContactUsView'),
+    path('contact/<int:pk>', ContactUsView.ContactUsDetailedView.as_view(), name='ContactUsDetailedView'),
+
+]
